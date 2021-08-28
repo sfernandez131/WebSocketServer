02201a730cf7fc7b6e34f5e2254314ca11b5268a
Scott Fernandez <sfernandez131@gmail.com>
2021-08-28 16:18:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocketSrvrs
+{
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Recieved message from client: " + e.Data);
+            Send(e.Data);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            WebSocketServer wssv = new("ws://127.0.0.1:7889");
+
+            wssv.AddWebSocketService<Echo>("/Echo");
+
+            wssv.Start();
+            Console.WriteLine($"WebSocketServer started on ws://127.0.0.1:7889/Echo");
+
+            Console.ReadKey();
+            wssv.Stop();
+        }
+    }
+}
